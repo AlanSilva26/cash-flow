d7c94f59e21c38ec81fbf8a08b5abc470e3f1cfa
Alan Silva <alanmdo2008@gmail.com>
2026-08-20 22:35:12 -0400

    chore: configure OpenAPI documentation

--- src/Transaction/CashFlow.Transaction.Api/Program.cs
+++ src/Transaction/CashFlow.Transaction.Api/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "CashFlow Transaction API v1");
+    });
+}
+
+app.UseHttpsRedirection();
 
 app.Run();
